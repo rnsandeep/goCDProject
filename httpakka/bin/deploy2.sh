e88092763346a481052438849e8ff592bbb271c3
rnsandeep <nagasandeep.ramachandruni@avalara.com>
2020-08-26 18:20:30 +0530

    took version as input from environment

--- httpakka/bin/deploy2.sh
+++ httpakka/bin/deploy2.sh
@@ -1,6 +1,6 @@
 set -ex
 project=my-chart
-helm install $project ops/helm --values ops/helm/values.yaml
+helm install --set version=$GO_PIPELINE_LABEL $project ops/helm --values ops/helm/values.yaml
 sleep 30
 export POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=bootcamp,app.kubernetes.io/instance=$project" -o jsonpath="{.items[0].metadata.name}")
 kubectl --namespace default port-forward $POD_NAME 8080:8080
