f114784d1b6870f45c0d1ef08491cc07941db98b
rnsandeep <nagasandeep.ramachandruni@avalara.com>
2020-08-26 17:09:20 +0530

    added code for helm config and yaml files for deployment

---
+++ httpakka/bin/deploy2.sh
@@ -0,0 +1,6 @@
+project=my-chart
+helm install $project ops/helm --values ops/helm/values.yaml
+sleep 30
+export POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=bootcamp,app.kubernetes.io/instance=$project" -o jsonpath="{.items[0].metadata.name}")
+kubectl --namespace default port-forward $POD_NAME 8080:8080
+
