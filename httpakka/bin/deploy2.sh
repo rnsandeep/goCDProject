a17ff798421c8b65917468a08ff5615068744bd6
rnsandeep <nagasandeep.ramachandruni@avalara.com>
2020-08-27 16:52:39 +0530

    added kubectl rollout to wait

--- httpakka/bin/deploy2.sh
+++ httpakka/bin/deploy2.sh
@@ -1,5 +1,8 @@
 set -ex
 project=my-chart
 helm install --wait --set version=$GO_PIPELINE_LABEL $project ops/helm --timeout 300s --values ops/helm/values.yaml
+for deployment in $(kubectl get deployments --namespace "$namespace" --output jsonpath='{.items[*].metadata.name}'); do
+            kubectl rollout status "deployment/$deployment" --namespace "default"
 sleep 30
 export POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=bootcamp,app.kubernetes.io/instance=$project" -o jsonpath="{.items[0].metadata.name}")
+
