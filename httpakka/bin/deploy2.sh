4f742f8ee53b22acb9eb2e1e8c716fd4c8e4e249
rnsandeep <nagasandeep.ramachandruni@avalara.com>
2020-08-27 13:55:11 +0530

    added port forwarding

--- httpakka/bin/deploy2.sh
+++ httpakka/bin/deploy2.sh
@@ -3,3 +3,4 @@ project=my-chart
 helm install --set version=$GO_PIPELINE_LABEL $project ops/helm --values ops/helm/values.yaml
 sleep 30
 export POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=bootcamp,app.kubernetes.io/instance=$project" -o jsonpath="{.items[0].metadata.name}")
+kubectl --namespace default port-forward $POD_NAME 8080:80 &
