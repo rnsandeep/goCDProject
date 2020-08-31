c52c3e00776161d056e6e68ec0af5b5e3d140b0c
rnsandeep <nagasandeep.ramachandruni@avalara.com>
2020-08-31 11:58:09 +0530

    removed wait

--- httpakka/bin/deploy2.sh
+++ httpakka/bin/deploy2.sh
@@ -1,9 +1,5 @@
 set -ex
 project=my-chart
-helm install --wait --set version=$GO_PIPELINE_LABEL $project ops/helm --timeout 300s --values ops/helm/values.yaml
-for deployment in $(kubectl get deployments --namespace "$namespace" --output jsonpath='{.items[*].metadata.name}'); 
-do
-            kubectl rollout status "deployment/$deployment" --namespace "default"
-done
+helm install --set version=$GO_PIPELINE_LABEL $project ops/helm  --values ops/helm/values.yaml
 sleep 30
 export POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=bootcamp,app.kubernetes.io/instance=$project" -o jsonpath="{.items[0].metadata.name}")
