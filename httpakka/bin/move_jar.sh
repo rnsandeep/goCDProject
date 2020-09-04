5c8c96f16c7153e8a98a9f2977b9561814298be8
rnsandeep <nagasandeep.ramachandruni@avalara.com>
2020-09-04 16:12:46 +0530

    changed to assembly.jar

--- httpakka/bin/move_jar.sh
+++ httpakka/bin/move_jar.sh
@@ -1,4 +1,4 @@
 echo ${GO_PIPELINE_LABEL}
 #ls target/scala-2.11/
 cat src/main/scala/httpakka.scala
-mv target/scala-2.11/httpakka-assembly-${GO_PIPELINE_LABEL}.jar .
+mv target/scala-2.11/httpakka-assembly-${GO_PIPELINE_LABEL}.jar assembly.jar
