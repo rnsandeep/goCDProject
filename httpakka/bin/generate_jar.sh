bfbc79af808f84604c8b0eccb44d816ce26b8b98
rnsandeep <nagasandeep.ramachandruni@avalara.com>
2020-09-04 14:38:55 +0530

    moved jar file to root folder

--- httpakka/bin/generate_jar.sh
+++ httpakka/bin/generate_jar.sh
@@ -1,3 +1,3 @@
 echo ${GO_PIPELINE_LABEL}
-ls target/scala-2.11/
-# mv httpakka/target/scala-2.11/httpakka-assembly-default.jar assembly_latest.jar
+#ls target/scala-2.11/
+mv target/scala-2.11/httpakka-assembly-${GO_PIPELINE_LABEL}.jar assembly_latest.jar
